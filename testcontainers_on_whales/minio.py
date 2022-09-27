94d103211f4db1c8dced06fc9fbfdab09ce9d7c2
Christoph Becker <mail@ch-becker.de>
2022-09-27 10:44:32 +0200

    feat: add minio extra env

--- testcontainers_on_whales/minio.py
+++ testcontainers_on_whales/minio.py
@@ -2,6 +2,8 @@ import boto3 as boto3
 import requests as requests
 import urllib3 as urllib3
 from botocore.client import Config
+from minio import Minio
+from minio.notificationconfig import NotificationConfig, QueueConfig
 
 from testcontainers_on_whales import Container
 
@@ -9,13 +11,14 @@ from testcontainers_on_whales import Container
 class MinioContainer(Container):
     MINIO_PORT = 9000
     MINIO_CONSOLE_PORT = 9001
+    AMQP_IDENTIFIER = "TEST"
 
     def __init__(
-        self,
-        image: str = "docker.io/minio/minio:latest",
-        username: str = "minioadmin",
-        password: str = "minioadmin",
-        extra_env: dict = None,
+            self,
+            image: str = "docker.io/minio/minio:latest",
+            username: str = "minioadmin",
+            password: str = "minioadmin",
+            extra_env: dict = None,
     ):
         self.username = username
         self.__password = password
@@ -30,10 +33,25 @@ class MinioContainer(Container):
                 f":{MinioContainer.MINIO_CONSOLE_PORT}",
             ],
             env={
-                "MINIO_ROOT_USER": self.username,
-                "MINIO_ROOT_PASSWORD": self.__password,
-            } | extra_env,
+                    "MINIO_ROOT_USER": self.username,
+                    "MINIO_ROOT_PASSWORD": self.__password,
+                } | extra_env,
         )
+
+    @staticmethod
+    def get_amqp_extra_env(amqp_url: str, exchange: str):
+        # "TEST" is the AMQP endpoint identifier
+        identifier = ""
+        if MinioContainer.AMQP_IDENTIFIER:
+            identifier = f"_{MinioContainer.AMQP_IDENTIFIER}"
+        return {
+            f"MINIO_NOTIFY_AMQP_ENABLE{identifier}": "on",
+            f"MINIO_NOTIFY_AMQP_URL{identifier}": amqp_url,  # amqp://guest:guest@rabbitmq:5672
+            f"MINIO_NOTIFY_AMQP_EXCHANGE{identifier}": exchange,
+            f"MINIO_NOTIFY_AMQP_EXCHANGE_TYPE{identifier}": "fanout",
+            f"MINIO_NOTIFY_AMQP_DURABLE{identifier}": "on",
+            f"MINIO_SKIP_CLIENT": "yes",  # bitnami minio client broke some setups in the past
+        }
 
     def get_connection_url(self) -> str:
         ip = self.get_container_ip()
@@ -72,3 +90,45 @@ class MinioContainer(Container):
         except requests.exceptions.ConnectionError:
             pass
         return False
+
+    def _get_arn(self):
+        idf = "_"
+        if self.AMQP_IDENTIFIER:
+            idf = self.AMQP_IDENTIFIER
+
+        return f"arn:minio:sqs::{idf}:amqp"
+
+    def get_minio_client(self):
+        ip = self.get_container_ip()
+        port = self.get_container_port(self.MINIO_PORT)
+        minio_endpoint = f"{ip}:{port}"
+        client = Minio(
+            minio_endpoint,
+            secure=False,
+            access_key=self.username,
+            secret_key=self.__password
+        )
+        return client
+
+    def configure_notifications(self, bucket_name: str, event_list: list[str]):
+        """
+        Configure this minio server to release events via amqp for specific S3 events
+        :param bucket_name:
+        :param event_list: e.g. ["s3:ObjectCreated:*"]
+        :return:
+        """
+        arn = self._get_arn()
+        client = self.get_minio_client()
+
+        notification_config = NotificationConfig(
+            queue_config_list=[
+                QueueConfig(
+                    arn,
+                    event_list,
+                    config_id="1",
+                    # prefix_filter_rule=PrefixFilterRule("abc"),
+                    # suffix_filter_rule=SuffixFilterRule(".pdf"),
+                ),
+            ],
+        )
+        client.set_bucket_notification(bucket_name, notification_config)

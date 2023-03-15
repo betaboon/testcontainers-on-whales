4c1447528c675cc11ec0d105d107905c605f7038
betaboon <betaboon@0x80.ninja>
2023-03-15 20:36:31 +0100

    test(postgresql): adapt for sqlalchemy>=2.0

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -1,25 +1,27 @@
-from sqlalchemy import Column, Integer, MetaData, Table, Text, select
+from sqlalchemy import Column, Integer, MetaData, Table, Text, insert, select
 
 from testcontainers_on_whales.postgres import PostgresContainer
 
 
-def test_postgres_container():
+def test_postgres_container() -> None:
     with PostgresContainer() as container:
         container.wait_ready(timeout=120)
         engine = container.get_sqlalchemy_engine()
 
         metadata = MetaData()
-        messages = Table(
+        messages_table = Table(
             "messages",
             metadata,
             Column("id", Integer, primary_key=True),
             Column("message", Text),
         )
-        messages.create(bind=engine)
+        messages_table.create(bind=engine)
 
-        insert_message = messages.insert().values(message="Hello, Test!")
-        engine.execute(insert_message)
-        stmt = select([messages.c.message])
-        (message,) = engine.execute(stmt).fetchone()
-
-        assert message == "Hello, Test!"
+        with engine.connect() as connection:
+            insert_message = insert(messages_table).values(message="Hello, Test!")
+            connection.execute(insert_message)
+            select_stmt = select(messages_table)
+            message_row = connection.execute(select_stmt).fetchone()
+            assert message_row is not None
+            _, message = message_row
+            assert message == "Hello, Test!"
